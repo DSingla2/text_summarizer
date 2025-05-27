e3386ba36a8183e0dcdae199d85ed8c86c025a57
Deepanhi <singladeepanshi6@gmail.com>
2025-05-27 17:12:05 +0530

    Initial commit

---
+++ Text_summarization.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from transformers import pipeline
+st.title("Text_Summarizer")
+@st.cache_resource
+def load_summarizer():
+    return pipeline("summarization", model="facebook/bart-large-cnn")
+
+summarizer = load_summarizer()
+
+#User Input
+text= st.text_area("Paste your paragraph here: ")
+
+if st.button("Summarize"): 
+    if not text.strip(): 
+        st.warning("Please enter some text.")
+    else: 
+        summary = summarizer(text, max_length= 100)
+        st.subheader("Summary: ")
+        st.write(summary[0]['summary_text'])
